371020cb0e84a076b64bd463ed3735a9d85954f3
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2022-03-02 03:29:18 +0300

    Source code directory

---
+++ src/community.js
@@ -0,0 +1,12 @@
+class Neighbour {
+    moveTo(distance){
+
+    }
+    announce(message){
+
+    }
+    heardAnnouncement(){
+        return ["My restruant is now open for business"]
+    }
+}
+module.exports = Neighbour;
